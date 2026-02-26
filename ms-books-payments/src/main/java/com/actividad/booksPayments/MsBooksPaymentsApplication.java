a55d08077e16c4f3b8cdfedd9d45d69b2912be69
Jordan Vera <jordan.vera667@comunidadunir.net>
2026-02-25 22:05:28 -0500

    se ajusta validacion de profile para payments

--- ms-books-payments/src/main/java/com/actividad/booksPayments/MsBooksPaymentsApplication.java
+++ ms-books-payments/src/main/java/com/actividad/booksPayments/MsBooksPaymentsApplication.java
@@ -1,19 +1,31 @@
 package com.actividad.booksPayments;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@Slf4j
 public class MsBooksPaymentsApplication {
 
     @Bean
     public WebClient.Builder webClient(){return WebClient.builder();}
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsBooksPaymentsApplication.class, args);
+        // Retrieve execution profile from environment variable. If not present, default profile is selected.
+        String profile = System.getenv("PROFILE");
+        System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+        // Railway's internal interface takes some time to start. We wait for it to be ready.
+        log.debug("Waiting fo Internal Interface to start...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        SpringApplication.run(MsBooksPaymentsApplication.class, args);
 	}
 
 }
